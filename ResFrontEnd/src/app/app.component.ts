e664843f9a8f3c93731d93aa844b219b617557e7
redeemer <redeemermatshili@thedigitalacademy.co.za>
2019-07-19 15:05:21 +0200

    playing with side menu

--- ResFrontEnd/src/app/app.component.ts
+++ ResFrontEnd/src/app/app.component.ts
@@ -11,13 +11,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 export class AppComponent {
   public appPages = [
     {
-      title: 'Home',
-      url: '/home',
+      // title: 'Home',
+      // url: '/home',
       icon: 'home'
     },
     {
-      title: 'List',
-      url: '/list',
+      // title: 'List',
+      // url: '/list',
       icon: 'list'
     }
   ];
